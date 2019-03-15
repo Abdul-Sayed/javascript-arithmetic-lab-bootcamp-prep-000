34a9f7decba1baa922ca6a1d57a4138b8cbe5963
Abdul <awsayed@alaska.edu>
2019-03-15 20:21:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,41 +1,8 @@
-var number = 10
- 
-function add5() {
-  number += 5
+function add(a,b) {
+  return a + b;
 }
- 
-function divideBy3() {
-  number /= 3
+
+function subtract(a,b) {
+  return a - b;
 }
- 
-divideBy3()
- 
-console.log(number) // 3.333333333335
- 
-add5()
- 
-console.log(number) // 8.333333333335
- 
-// reset number
-number = 10
- 
-add5()
- 
-console.log(number) // 15
- 
-divideBy3()
- 
-console.log(number) // 5
 
-
-
-parseInt('2', 10) // 2
-
-
-parseInt('2.2222', 10)
-
-
-parseInt('nonsense!', 10)
-
-
-parseFloat('80.123999') // 80.123999
